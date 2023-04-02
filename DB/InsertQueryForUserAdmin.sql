c7ce21624bd51e4cd71bf316fec78e1e6b2bcab4
justinegeo96 <justinegeo96@gmail.com>
2023-04-02 17:46:32 -0500

    refactor

--- DB/InsertQueryForUserAdmin.sql
+++ DB/InsertQueryForUserAdmin.sql
@@ -1,69 +1,74 @@
-INSERT INTO user(UserId,Email,Phone,FName,LName,CreateDate,AccountPassword) VALUES 
-(1001,'mauris@outlook.com','(387) 978-2131','Oprah','Dawson','2022-05-05 10:06:54','IYS77MCF6TP'),
-(1002,'gravida.sit@outlook.com','(260) 828-1692','Cairo','Clay','2022-05-30 14:08:59','BNH93CMD8GN'),
-(1003,'at.sem.molestie@outlook.com','(721) 849-7251','Stephen','Sandoval','2022-10-30 08:16:03','EWT37PRK4TU'),
-(1004,'ultricies@outlook.com','(204) 926-1861','Diana','Franks','2022-06-10 21:14:45','UJO45IWI6TE'),
-(1005,'rutrum.fusce@gmail.com','(217) 363-5631','Maile','Herring','2023-02-03 19:50:10','OQW53ZCU5VG'),
-(1006,'mauris.blandit.enim@hotmail.com','(431) 859-6765','Samantha','Roy','2022-09-07 05:23:17','AXN89JJT6YT'),
-(1007,'nisi.mauris@hotmail.com','(422) 628-3144','Jarrod','Cummings','2022-09-05 03:49:49','SLD57CXE7VK'),
-(1008,'condimentum.eget@gmail.com','(804) 657-1617','Melissa','Stevens','2023-04-01 03:36:20','YWQ01JNL5ZB'),
-(1009,'aliquet.metus@gmail.com','(788) 881-2823','Lenore','Fisher','2022-12-22 15:04:26','CFU25AHY2UR'),
-(1010,'primis.in@hotmail.com','(201) 646-5213','Cameron','Morgan','2023-01-01 05:02:13','JEF88GNR6IC'),
-(1011,'et@hotmail.com','(384) 887-5653','Mira','Knapp','2022-10-02 02:05:03','MTX14SQL5OE'),
-(1012,'posuere.cubilia.curae@yahoo.com','(592) 251-9046','Pearl','Carter','2023-01-01 23:29:59','WWO31JBB2SY'),
-(1013,'pede.nunc@gmail.com','(582) 626-3128','Damian','Combs','2023-01-04 03:57:20','KCU43LXG6NV'),
-(1014,'est.nunc@yahoo.com','(312) 578-1387','Shelby','Larson','2022-04-22 20:56:32','YMU59ZBT6WG'),
-(1015,'libero.morbi@yahoo.com','(401) 765-6220','Derek','Robles','2022-11-01 08:30:22','SZJ13REJ8MK'),
-(1016,'tempor.bibendum@gmail.com','(546) 218-4255','Buckminster','Bowman','2022-05-09 16:19:18','JVF07TAC5QP'),
-(1017,'ultrices.posuere@hotmail.com','(248) 456-3233','Ivory','Meyers','2022-09-20 21:05:12','JWX65MCS6IN'),
-(1018,'sem.magna@yahoo.com','(578) 521-7497','Jesse','Bowers','2022-07-07 15:52:24','ONM39ETQ1CF'),
-(1019,'elit.curabitur@yahoo.com','(167) 624-6261','Callum','Britt','2022-06-30 23:29:04','QCO83UIW9XZ'),
-(1020,'sit@hotmail.com','(667) 556-5059','Jemima','Stein','2022-05-24 23:48:38','ZCY58YRM2LF'),
-(1021,'sapien.molestie.orci@gmail.com','(726) 301-0831','Jerome','Wyatt','2023-03-12 11:03:29','WNG35ICI4VD'),
-(1022,'enim.sit.amet@outlook.com','(771) 858-8245','Scott','Odom','2022-05-12 09:53:27','HCU48TSZ1FA'),
-(1023,'nulla.ante@outlook.com','(419) 898-8861','Channing','Finch','2023-03-01 07:44:59','WDT34ASI9CI'),
-(1024,'fusce.dolor@gmail.com','(323) 456-2013','Cyrus','Frost','2022-10-22 04:08:39','DEF55FHZ6VY'),
-(1025,'sed.leo@gmail.com','(285) 326-4566','Rhoda','Villarreal','2022-11-05 10:11:48','DGB76GQE2PK'),
-(1026,'tellus.aenean.egestas@gmail.com','(647) 840-7446','Joseph','Stark','2022-09-10 04:11:15','LRO52PJT4SO'),
-(1027,'adipiscing@outlook.com','(678) 781-8773','Rina','Clemons','2023-02-05 00:34:54','CTW57IJQ9LH'),
-(1028,'nunc@outlook.com','(459) 256-5424','Britanni','Clarke','2022-09-17 23:38:18','JXT13UEJ0DI'),
-(1029,'elit.dictum.eu@hotmail.com','(937) 412-5771','Pamela','Carr','2022-12-25 02:18:18','TPS82SFM8PR'),
-(1030,'nam.nulla@yahoo.com','(464) 101-1873','Tatiana','Mooney','2023-01-11 11:19:08','LNH87MDF6MC');
+INSERT INTO User (UserId, Email, Phone, FName, LName, CreateDate, AccountPassword)
+VALUES
+  (1001, 'mauris@outlook.com', '(387) 978-2131', 'Oprah', 'Dawson', '2022-05-05 10:06:54', 'IYS77MCF6TP'),
+  (1002, 'gravida.sit@outlook.com', '(260) 828-1692', 'Cairo', 'Clay', '2022-05-30 14:08:59', 'BNH93CMD8GN'),
+  (1003, 'at.sem.molestie@outlook.com', '(721) 849-7251', 'Stephen', 'Sandoval', '2022-10-30 08:16:03', 'EWT37PRK4TU'),
+  (1004, 'ultricies@outlook.com', '(204) 926-1861', 'Diana', 'Franks', '2022-06-10 21:14:45', 'UJO45IWI6TE'),
+  (1005, 'rutrum.fusce@gmail.com', '(217) 363-5631', 'Maile', 'Herring', '2023-02-03 19:50:10', 'OQW53ZCU5VG'),
+  (1006, 'mauris.blandit.enim@hotmail.com', '(431) 859-6765', 'Samantha', 'Roy', '2022-09-07 05:23:17', 'AXN89JJT6YT'),
+  (1007, 'nisi.mauris@hotmail.com', '(422) 628-3144', 'Jarrod', 'Cummings', '2022-09-05 03:49:49', 'SLD57CXE7VK'),
+  (1008, 'condimentum.eget@gmail.com', '(804) 657-1617', 'Melissa', 'Stevens', '2023-04-01 03:36:20', 'YWQ01JNL5ZB'),
+  (1009, 'aliquet.metus@gmail.com', '(788) 881-2823', 'Lenore', 'Fisher', '2022-12-22 15:04:26', 'CFU25AHY2UR'),
+  (1010, 'primis.in@hotmail.com', '(201) 646-5213', 'Cameron', 'Morgan', '2023-01-01 05:02:13', 'JEF88GNR6IC'),
+  (1011, 'et@hotmail.com', '(384) 887-5653', 'Mira', 'Knapp', '2022-10-02 02:05:03', 'MTX14SQL5OE'),
+  (1012, 'posuere.cubilia.curae@yahoo.com', '(592) 251-9046', 'Pearl', 'Carter', '2023-01-01 23:29:59', 'WWO31JBB2SY'),
+  (1013, 'pede.nunc@gmail.com', '(582) 626-3128', 'Damian', 'Combs', '2023-01-04 03:57:20', 'KCU43LXG6NV'),
+  (1014, 'est.nunc@yahoo.com', '(312) 578-1387', 'Shelby', 'Larson', '2022-04-22 20:56:32', 'YMU59ZBT6WG'),
+  (1015, 'libero.morbi@yahoo.com', '(401) 765-6220', 'Derek', 'Robles', '2022-11-01 08:30:22', 'SZJ13REJ8MK'),
+  (1016, 'tempor.bibendum@gmail.com', '(546) 218-4255', 'Buckminster', 'Bowman', '2022-05-09 16:19:18', 'JVF07TAC5QP'),
+  (1017, 'ultrices.posuere@hotmail.com', '(248) 456-3233', 'Ivory', 'Meyers', '2022-09-20 21:05:12', 'JWX65MCS6IN'),
+  (1018, 'sem.magna@yahoo.com', '(578) 521-7497', 'Jesse', 'Bowers', '2022-07-07 15:52:24', 'ONM39ETQ1CF'),
+  (1019, 'elit.curabitur@yahoo.com', '(167) 624-6261', 'Callum', 'Britt', '2022-06-30 23:29:04', 'QCO83UIW9XZ'),
+  (1020, 'sit@hotmail.com', '(667) 556-5059', 'Jemima', 'Stein', '2022-05-24 23:48:38', 'ZCY58YRM2LF'),
+  (1021, 'sapien.molestie.orci@gmail.com', '(726) 301-0831', 'Jerome', 'Wyatt', '2023-03-12 11:03:29', 'WNG35ICI4VD'),
+  (1022, 'enim.sit.amet@outlook.com', '(771) 858-8245', 'Scott', 'Odom', '2022-05-12 09:53:27', 'HCU48TSZ1FA'),
+  (1023, 'nulla.ante@outlook.com', '(419) 898-8861', 'Channing', 'Finch', '2023-03-01 07:44:59', 'WDT34ASI9CI'),
+  (1024, 'fusce.dolor@gmail.com', '(323) 456-2013', 'Cyrus', 'Frost', '2022-10-22 04:08:39', 'DEF55FHZ6VY'),
+  (1025, 'sed.leo@gmail.com', '(285) 326-4566', 'Rhoda', 'Villarreal', '2022-11-05 10:11:48', 'DGB76GQE2PK'),
+  (1026, 'tellus.aenean.egestas@gmail.com', '(647) 840-7446', 'Joseph', 'Stark', '2022-09-10 04:11:15', 'LRO52PJT4SO'),
+  (1027, 'adipiscing@outlook.com', '(678) 781-8773', 'Rina', 'Clemons', '2023-02-05 00:34:54', 'CTW57IJQ9LH'),
+  (1028, 'nunc@outlook.com', '(459) 256-5424', 'Britanni', 'Clarke', '2022-09-17 23:38:18', 'JXT13UEJ0DI'),
+  (1029, 'elit.dictum.eu@hotmail.com', '(937) 412-5771', 'Pamela', 'Carr', '2022-12-25 02:18:18', 'TPS82SFM8PR'),
+  (1030, 'nam.nulla@yahoo.com', '(464) 101-1873', 'Tatiana', 'Mooney', '2023-01-11 11:19:08', 'LNH87MDF6MC');
 
+INSERT INTO Customer (CustomerId)
+VALUES
+  (1002),
+  (1003),
+  (1005),
+  (1006),
+  (1009),
+  (1011),
+  (1012),
+  (1015),
+  (1016),
+  (1017),
+  (1018),
+  (1019),
+  (1021),
+  (1023),
+  (1024),
+  (1025),
+  (1026),
+  (1027),
+  (1029),
+  (1030);
 
-INSERT INTO customer(CustomerId) VALUES (1002);
-INSERT INTO customer(CustomerId) VALUES (1003);
-INSERT INTO customer(CustomerId) VALUES (1005);
-INSERT INTO customer(CustomerId) VALUES (1006);
-INSERT INTO customer(CustomerId) VALUES (1009);
-INSERT INTO customer(CustomerId) VALUES (1011);
-INSERT INTO customer(CustomerId) VALUES (1012);
-INSERT INTO customer(CustomerId) VALUES (1015);
-INSERT INTO customer(CustomerId) VALUES (1016);
-INSERT INTO customer(CustomerId) VALUES (1017);
-INSERT INTO customer(CustomerId) VALUES (1018);
-INSERT INTO customer(CustomerId) VALUES (1019);
-INSERT INTO customer(CustomerId) VALUES (1021);
-INSERT INTO customer(CustomerId) VALUES (1023);
-INSERT INTO customer(CustomerId) VALUES (1024);
-INSERT INTO customer(CustomerId) VALUES (1025);
-INSERT INTO customer(CustomerId) VALUES (1026);
-INSERT INTO customer(CustomerId) VALUES (1027);
-INSERT INTO customer(CustomerId) VALUES (1029);
-INSERT INTO customer(CustomerId) VALUES (1030);
+INSERT INTO AdminUser (AdminId)
+VALUES
+    (1001);
 
-INSERT INTO AdminUser(AdminId) VALUES (1001);
-
-INSERT INTO Vendor(VendorId , StoreAddress) VALUES 
-(1004, "Ap #141-9353 Nibh Rd.,Caldera,38562,NE" ),
-(1007 ,"7649 Praesent Rd.,Chancay,12933 ,Arizona"),
-(1008 , "Ap #535-5448 Vel Avenue,Montpellier,92374,MA"),
-(1010 , "Ap #188-7120 Aliquet Avenue,Enns,58742,MI"),
-(1013,"541-7668 Lacus. Rd.,Dover,36589,TX"),
-(1014,"Ap #397-9638 Lacus. Rd.,Richmond,82252,Colorado"),
-(1020,"Ap #431-558 Velit Rd.,Bludenz,99741,MD"),
-(1022, "847-357 Tempus Rd.,Birori,53858,OR"),
-(1028, "499-2828 Fames St.,Chiclayo,52294,KS");
+INSERT INTO Vendor (VendorId, StoreAddress)
+VALUES
+  (1004, "Ap #141-9353 Nibh Rd., Caldera, 38562, NE" ),
+  (1007, "7649 Praesent Rd., Chancay, 12933 , Arizona"),
+  (1008, "Ap #535-5448 Vel Avenue, Montpellier, 92374, MA"),
+  (1010, "Ap #188-7120 Aliquet Avenue, Enns, 58742, MI"),
+  (1013, "541-7668 Lacus. Rd., Dover, 36589, TX"),
+  (1014, "Ap #397-9638 Lacus. Rd., Richmond, 82252, Colorado"),
+  (1020, "Ap #431-558 Velit Rd., Bludenz, 99741, MD"),
+  (1022, "847-357 Tempus Rd., Birori, 53858, OR"),
+  (1028, "499-2828 Fames St., Chiclayo, 52294, KS");
 
 INSERT INTO CustomerAddress (CustomerAddressId, CustomerId, AddressLine1, AddressLine2, City, State, Zip, IsPrimary)
 VALUES
