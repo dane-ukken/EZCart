021d52a69ead05029536956bbad3b1563a8179f0
Garima Rahangdale <63338418+Garima2997@users.noreply.github.com>
2023-04-26 01:03:24 -0500

    added methods for Admin

--- Backend/com/ezcart/dao/IProductDao.java
+++ Backend/com/ezcart/dao/IProductDao.java
@@ -10,6 +10,10 @@ public interface IProductDao {
 	public List<Product> getProductSubCategory();
 	public List<Product> getAllProducts();
 	public List<Product> getProductBySubcategory(int subcategoryId);
-	public List<Product> getSubcategoryByCategory(int categoryId);
+	public List<Product> getSubcategoryByCategory(int categoryId);	
+	public int addProduct(ProductDTO product);
+	public int deleteProduct(int productId);
+	public int updateProduct(ProductDTO product);
+	public int addVendorForNewProduct(ProductDTO product , int productId);
 	
 }
