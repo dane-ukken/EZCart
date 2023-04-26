c2f3c254b57caf9d89989588c134ec0b5d484966
Garima Rahangdale <63338418+Garima2997@users.noreply.github.com>
2023-04-26 12:13:15 -0500

    Update ProductServiceImpl.java

--- Backend/com/ezcart/service/ProductServiceImpl.java
+++ Backend/com/ezcart/service/ProductServiceImpl.java
@@ -62,5 +62,83 @@ public class ProductServiceImpl implements IProductService {
 	public int updateProduct(ProductDTO product) {
 		return prodDao.updateProduct(product);
 	}
+	
 
+}
+package com.ezcart.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ezcart.dao.IProductDao;
+import com.ezcart.dto.ProductDTO;
+import com.ezcart.dto.ProductDetailsDTO;
+import com.ezcart.pojos.Product;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements IProductService {
+
+	@Autowired
+	private IProductDao prodDao;
+	
+	
+	@Override
+	public List<Product> getProductCategory() {
+		return prodDao.getProductCategory();
+	}
+
+
+	@Override
+	public List<Product> getSubProductCategory() {
+		return prodDao.getProductSubCategory();
+	}
+
+
+	@Override
+	public List<Product> getAllProducts() {
+		return prodDao.getAllProducts();
+	}
+
+
+	@Override
+	public List<Product> getProductBySubcategory(int subcategoryId) {
+		return prodDao.getProductBySubcategory(subcategoryId);
+	}
+
+
+	@Override
+	public List<Product> getSubcategoryByCategory(int categoryId) {
+		return prodDao.getSubcategoryByCategory(categoryId);
+	}
+
+
+	@Override
+	public int addProduct(ProductDTO product) {
+		return prodDao.addProduct(product);
+	}
+
+
+	@Override
+	public int deleteProduct(int productId) {
+		return prodDao.deleteProduct(productId);
+	}
+
+
+	@Override
+	public int updateProduct(ProductDTO product) {
+		return prodDao.updateProduct(product);
+	}
+
+
+	@Override
+	public List<ProductDetailsDTO>getProductById(int productId) {
+		return prodDao.getProductById(productId);
+	}
+	
+	
+	
 }
