6a2f3b2bc88ad79f30013210dcd0fe41942cc89e
Garima Rahangdale <63338418+Garima2997@users.noreply.github.com>
2023-04-26 01:10:28 -0500

    Update IProductService.java

--- Backend/com/ezcart/service/IProductService.java
+++ Backend/com/ezcart/service/IProductService.java
@@ -11,5 +11,8 @@ public interface IProductService {
 	public List<Product> getAllProducts();
 	public List<Product> getProductBySubcategory(int subcategoryId);
 	public List<Product> getSubcategoryByCategory(int categoryId);
+	public int addProduct(ProductDTO product);
+	public int deleteProduct(int productId);
+	public int updateProduct(ProductDTO product);
 
 }
