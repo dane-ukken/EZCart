437eafd9fe86deb363fc97e27cdbff78ed790bcf
Garima Rahangdale <63338418+Garima2997@users.noreply.github.com>
2023-04-26 01:09:24 -0500

    Add files via upload

---
+++ Backend/com/ezcart/pojos/Vendor.java
@@ -0,0 +1,28 @@
+package com.ezcart.pojos;
+
+public class Vendor {
+
+	private int VendorID;
+	private String StoreAddress;
+
+	public int getVendorID() {
+		return VendorID;
+	}
+
+	public void setVendorID(int VendorID) {
+		this.VendorID = VendorID;
+	}
+
+	public String getStoreAddress() {
+		return StoreAddress;
+	}
+
+	public void setStoreAddress(String StoreAddress) {
+		this.StoreAddress = StoreAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "Vendor [VendorID=" + VendorID + ", StoreAddress=" + StoreAddress + "]";
+	}
+}
